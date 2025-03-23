60d9a7d282914eb2a9889fcde2055c29eea759b6
Elizabeth Thomas <elizabeth.thomas2@georgebrown.ca>
2025-03-23 14:40:32 -0400

    Initial Commit

---
+++ A2_iOS_elizabeth_101097106/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_elizabeth_101097106
+//
+//  Created by Elizabeth Thomas on 2025-03-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
